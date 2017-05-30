eb96be48224bd1e627a9838737da50ca7b6199a5
Benjamin Wang <ben.kb.wang@gmail.com>
2017-05-30 07:05:32 +0000

    Added basic header and footer and  home, help, about routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
   root 'static_pages#home'
+ 
+  # get 'static_pages/about' -> this takes you to http://www.example.com/static_pages/about
+  # use a named route instead; access by about_path or about_url
+  # help_path -> '/help'
+  # help_url  -> 'http://www.example.com/help'
+  get '/about', to: 'static_pages#about'
+  get '/help', to:  'static_pages#help'
 end
